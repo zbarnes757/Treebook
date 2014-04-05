e1a664bf1fb389c60168820ee4e19eb4d37f596f
Zac Barnes <zbarnes@qubiclemonkey.com>
2014-04-05 18:19:48 -0400

    Created scaffold for statuses and began Treebook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '173a116cf0e932dd6fca777afafbc059c45c774d5195cc6662b3b45c4244f556aa5cada72eac6a89b7b62a60bff3634dd4b8aa6de499bad14f5d9777bafdfd9f'
